dbe0e8824e9b99df5fe14d2e1df9729d4f7ef964
hermasyp <hermas.yuda@gmail.com>
2018-03-20 09:52:47 +0700

    Update TwitterConfig

--- TwitterConfig.example.py
+++ TwitterConfig.example.py
@@ -1,12 +1,12 @@
 
 import tweepy
 #Twitter Configuration
-CONSUMER_KEY    = 'ivZxDXj7bc26e1HUbBbVAFlwN'
-CONSUMER_SECRET = 'hI9YQeJo6FatoVNJyY4HZgjMic61dpFUPAo6DUkdVvl17IEoIC'
+CONSUMER_KEY    = ''
+CONSUMER_SECRET = ''
 
 # Access Configuration:
-ACCESS_TOKEN  = '68413536-WpYNwsQg1NwRq4rnUx6ZP332HOc7znHh297TCw3sk'
-ACCESS_SECRET = 'Eajelt5yz1NaYtHHGq0EOzRIv9Xk81JXWWmQ4U1ihfCjO'
+ACCESS_TOKEN  = ''
+ACCESS_SECRET = ''
 
 
 def twitter_init():
